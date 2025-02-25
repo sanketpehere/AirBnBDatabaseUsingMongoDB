099c79387bcbd767df5d75122ef6b796d1b3341f
WebDevilSanket <105560181+WebDevilSanket@users.noreply.github.com>
2025-02-25 18:38:25 +0530

    Fixed Database Issue

--- utils/databaseUtil.js
+++ utils/databaseUtil.js
@@ -9,30 +9,64 @@
 
 // module.exports = pool.promise();
 
+// const mongo = require("mongodb");
+
+// const MongoClient = mongo.MongoClient;
+
+// const MONGO_URL =
+//   "mongodb+srv://root:root@aribnb.fej75.mongodb.net/?retryWrites=true&w=majority&appName=aribnb&tls=true";
+
+// let _db;
+
+// const mongoConnect = (callback) => {
+//   MongoClient.connect(MONGO_URL)
+//     .then((client) => {
+//       console.log("Connected to mongo successfuly.");
+//       _db = client.db("airbnb");
+//       callback();
+//     })
+//     .catch((err) => {
+//       console.log("Error while connecting to mongo", err.message);
+//     });
+// };
+
+// const getDB = () => {
+//   if (!_db) {
+//     throw new Error("Mongo not connected");
+//   }
+//   return _db;
+// };
+
+// exports.mongoConnect = mongoConnect;
+// exports.getDB = getDB;
+
 const mongo = require("mongodb");
 
 const MongoClient = mongo.MongoClient;
 
 const MONGO_URL =
-  "mongodb+srv://root:root@aribnb.fej75.mongodb.net/?retryWrites=true&w=majority&appName=aribnb";
+  "mongodb+srv://root:root@aribnb.fej75.mongodb.net/airbnb?retryWrites=true&w=majority&appName=aribnb&tls=true";
 
 let _db;
 
 const mongoConnect = (callback) => {
-  MongoClient.connect(MONGO_URL)
+  MongoClient.connect(MONGO_URL, {
+    tls: true, // Keep TLS enabled
+    serverSelectionTimeoutMS: 5000, // Timeout if server is unresponsive
+  })
     .then((client) => {
-      console.log("Connected to mongo successfuly.");
-      _db = client.db("airbnb");
+      console.log("✅ Connected to MongoDB successfully.");
+      _db = client.db(); // Automatically picks the database from URI
       callback();
     })
     .catch((err) => {
-      console.log("Error while connecting to mongo", err.message);
+      console.error("❌ Error while connecting to MongoDB:", err.message);
     });
 };
 
 const getDB = () => {
   if (!_db) {
-    throw new Error("Mongo not connected");
+    throw new Error("MongoDB is not connected.");
   }
   return _db;
 };
